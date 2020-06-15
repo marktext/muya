51a6d1f273b041df5f10ccbc8c2f568674939be5
jocs <ransixi@gmail.com>
2020-06-16 02:08:16 +0800

    feat: emoji picker

--- example/index.js
+++ example/index.js
@@ -1,10 +1,21 @@
 import Muya from '../lib'
+import EmojiPicker from '../lib/ui/emojiPicker'
+
+Muya.use(EmojiPicker)
 
 const container = document.querySelector('#editor')
 const muya = new Muya(container)
+
 window.muya = muya
+
 muya.init()
+
 muya.on('json-change', changes => {
   console.log(JSON.stringify(muya.getState(), null, 2))
   console.log(JSON.stringify(changes, null, 2))
+})
+
+muya.on('selection-change', changes => {
+  const { anchor, focus, path } = changes
+  console.log(JSON.stringify([anchor.offset, focus.offset, path]))
 })
