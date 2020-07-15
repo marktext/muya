7afea66a31197857a7fd2f2227158fff5fb6346f
jocs <ransixi@gmail.com>
2020-07-15 23:53:45 +0800

    feat: add history module

--- example/index.js
+++ example/index.js
@@ -199,45 +199,46 @@ const DEFAULT_STATE = [
     text: '图片![](https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592848169049&di=1bf848686f738f8697ec90a2d484a29c&imgtype=0&src=http%3A%2F%2Fbpic.588ku.com%2Felement_pic%2F01%2F54%2F05%2F625746fd5b60878.jpg) bar &gt; *zar* <ruby>北京<rt>Beijing</rt></ruby> foo bar $a \\ne b$ 和自己'
   }
 ]
+const DEFAULT_MARKDOWN = 'foo bar'
 
-const DEFAULT_MARKDOWN = `
-foo barw
+// const DEFAULT_MARKDOWN = `
+// foo barw
 
-<div>
-foo bar
-</div>
+// <div>
+// foo bar
+// </div>
 
-| foo | bar     |
-| ---:| ------- |
-| zar | foo bar |
+// | foo | bar     |
+// | ---:| ------- |
+// | zar | foo bar |
 
-0. foo
-   bar
+// 0. foo
+//    bar
 
-- foo bar1
-  
-  foo bar2
+// - foo bar1
 
-- [ ] a
-- [x] b
-- [ ] c
-- [ ] d
+//   foo bar2
 
-**blod** *emphasis* :man: <u>underline</u> <mark>highlight</mark> \`inline code\`~~删除~~ [百度](http://www.baidu.com) http://google.com
+// - [ ] a
+// - [x] b
+// - [ ] c
+// - [ ] d
 
----
+// **blod** *emphasis* :man: <u>underline</u> <mark>highlight</mark> \`inline code\`~~删除~~ [百度](http://www.baidu.com) http://google.com
 
-# foo bar
+// ---
 
-foo
-bar
-===
+// # foo bar
 
-> foo
-> bar
+// foo
+// bar
+// ===
 
-图片![](https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592848169049&di=1bf848686f738f8697ec90a2d484a29c&imgtype=0&src=http%3A%2F%2Fbpic.588ku.com%2Felement_pic%2F01%2F54%2F05%2F625746fd5b60878.jpg) bar &gt; *zar* <ruby>北京<rt>Beijing</rt></ruby> foo bar $a \ne b$ 和自己
-`
+// > foo
+// > bar
+
+// 图片![](https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592848169049&di=1bf848686f738f8697ec90a2d484a29c&imgtype=0&src=http%3A%2F%2Fbpic.588ku.com%2Felement_pic%2F01%2F54%2F05%2F625746fd5b60878.jpg) bar &gt; *zar* <ruby>北京<rt>Beijing</rt></ruby> foo bar $a \ne b$ 和自己
+// `
 
 Muya.use(EmojiPicker)
 Muya.use(FormatPicker)
@@ -256,11 +257,21 @@ Muya.use(PreviewTools)
 console.log(DEFAULT_STATE)
 
 const container = document.querySelector('#editor')
+const undoBtn = document.querySelector('#undo')
+const redoBtn = document.querySelector('#redo')
 const muya = new Muya(container, { markdown: DEFAULT_MARKDOWN })
 
 window.muya = muya
 
 muya.init()
+
+undoBtn.addEventListener('click', () => {
+  muya.editor.history.undo()
+})
+
+redoBtn.addEventListener('click', () => {
+  muya.editor.history.redo()
+})
 
 muya.on('json-change', changes => {
   console.log(JSON.stringify(muya.getState(), null, 2))
