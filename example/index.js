b7d4a0337e5129f8e5b7a88a44d977d1262c6ac1
jocs <ransixi@gmail.com>
2020-07-26 11:27:15 +0800

    fix: backspace issue in table cell

--- example/index.js
+++ example/index.js
@@ -200,44 +200,44 @@ const DEFAULT_STATE = [
   }
 ]
 
-const DEFAULT_MARKDOWN = `
-foo barw
+// const DEFAULT_MARKDOWN = `
+// foo barw
 
-<div>
-foo bar
-</div>
+// <div>
+// foo bar
+// </div>
 
-| foo | bar     |
-| ---:| ------- |
-| zar | foo bar |
+// | foo | bar     |
+// | ---:| ------- |
+// | zar | foo bar |
 
-0. foo
-   bar
+// 0. foo
+//    bar
 
-- foo bar1
+// - foo bar1
 
-  foo bar2
+//   foo bar2
 
-- [ ] a
-- [x] b
-- [ ] c
-- [ ] d
+// - [ ] a
+// - [x] b
+// - [ ] c
+// - [ ] d
 
-**blod** *emphasis* :man: <u>underline</u> <mark>highlight</mark> \`inline code\`~~删除~~ [百度](http://www.baidu.com) http://google.com
+// **blod** *emphasis* :man: <u>underline</u> <mark>highlight</mark> \`inline code\`~~删除~~ [百度](http://www.baidu.com) http://google.com
 
----
+// ---
 
-# foo bar
+// # foo bar
 
-foo
-bar
-===
+// foo
+// bar
+// ===
 
-> foo
-> bar
+// > foo
+// > bar
 
-图片![](https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592848169049&di=1bf848686f738f8697ec90a2d484a29c&imgtype=0&src=http%3A%2F%2Fbpic.588ku.com%2Felement_pic%2F01%2F54%2F05%2F625746fd5b60878.jpg) bar &gt; *zar* <ruby>北京<rt>Beijing</rt></ruby> foo bar $a \ne b$ 和自己
-`
+// 图片![](https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592848169049&di=1bf848686f738f8697ec90a2d484a29c&imgtype=0&src=http%3A%2F%2Fbpic.588ku.com%2Felement_pic%2F01%2F54%2F05%2F625746fd5b60878.jpg) bar &gt; *zar* <ruby>北京<rt>Beijing</rt></ruby> foo bar $a \ne b$ 和自己
+// `
 
 Muya.use(EmojiPicker)
 Muya.use(FormatPicker)
@@ -264,7 +264,7 @@ const nextBtn = document.querySelector('#next')
 const replaceInput = document.querySelector('#replace')
 const singleBtn = document.querySelector('#single')
 const allBtn = document.querySelector('#all')
-const muya = new Muya(container, { markdown: DEFAULT_MARKDOWN })
+const muya = new Muya(container, { json: DEFAULT_STATE })
 
 window.muya = muya
 
@@ -303,7 +303,7 @@ allBtn.addEventListener('click', () => {
 muya.on('json-change', changes => {
   // console.log(JSON.stringify(muya.getState(), null, 2))
   // console.log(JSON.stringify(changes, null, 2))
-  // console.log(muya.getMarkdown())
+  console.log(muya.getMarkdown())
 })
 
 // muya.on('selection-change', changes => {
