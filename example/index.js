5af4254a91b97d5519f311ab52d207d7386dea15
jocs <ransixi@gmail.com>
2022-01-22 10:45:11 +0800

    feat: vega-lite

--- example/index.js
+++ example/index.js
@@ -15,11 +15,27 @@ import FrontButton from '../lib/ui/frontButton'
 
 const DEFAULT_STATE = [
   {
-    name: 'frontmatter',
-    text: 'title: marktext\nname: ransixi',
+    name: 'diagram',
+    text: `{
+      "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+      "description": "A simple bar chart with embedded data.",
+      "data": {
+        "values": [
+          {"a": "A", "b": 28}, {"a": "B", "b": 55}, {"a": "C", "b": 43},
+          {"a": "D", "b": 91}, {"a": "E", "b": 81}, {"a": "F", "b": 53},
+          {"a": "G", "b": 19}, {"a": "H", "b": 87}, {"a": "I", "b": 52}
+        ]
+      },
+      "mark": "bar",
+      "encoding": {
+        "x": {"field": "a", "type": "nominal", "axis": {"labelAngle": 0}},
+        "y": {"field": "b", "type": "quantitative"}
+      }
+    }
+    `,
     meta: {
-      lang: 'yaml', // yaml | toml | json
-      style: '-' // `-` for yaml | `+` for toml | `;;;` and `{}` for json
+      lang: 'json',
+      type: 'vega-lite'
     }
   },
   {
