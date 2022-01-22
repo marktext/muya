f6162c6b3d71f64cfeff8edf7ae3918abcc1571e
Ran Luo <ransixi@gmail.com>
2022-01-22 21:42:46 +0800

    build: update mermaid (#41)

--- example/index.js
+++ example/index.js
@@ -236,24 +236,16 @@ import FrontButton from '../lib/ui/frontButton'
 // }
 // ]
 
-const DEFAULT_MARKDOWN = `\`\`\`vega-lite
-{
-  "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
-  "description": "A simple bar chart with embedded data.",
-  "data": {
-    "values": [
-      {"a": "A", "b": 28}, {"a": "B", "b": 55}, {"a": "C", "b": 43},
-      {"a": "D", "b": 91}, {"a": "E", "b": 81}, {"a": "F", "b": 53},
-      {"a": "G", "b": 19}, {"a": "H", "b": 87}, {"a": "I", "b": 52}
-    ]
-  },
-  "mark": "bar",
-  "encoding": {
-    "x": {"field": "a", "type": "nominal", "axis": {"labelAngle": 0}},
-    "y": {"field": "b", "type": "quantitative"}
-  }
-}
-
+const DEFAULT_MARKDOWN = `\`\`\`mermaid
+stateDiagram-v2
+        State1: The state with a note
+        note right of State1
+            Important information! You can write
+            notes.
+        end note
+        State1 --> State2
+        note left of State2 : This is the note to the left.
+The state with a
 \`\`\`
 
 \`\`\`python
