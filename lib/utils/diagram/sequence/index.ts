8f9108911a082c6e00d748944a3be8deabb88abf
jocs <luoran1988@126.com>
2023-08-22 00:16:41 +0800

    refactor: turn UI to TS

---
+++ lib/utils/diagram/sequence/index.ts
@@ -0,0 +1,4 @@
+import Diagram from "@/assets/libs/sequence/sequence-diagram-snap";
+import "@/assets/styles/sequence-diagram.css";
+
+export default Diagram;
---
+++ lib/utils/turndownService/index.ts
@@ -0,0 +1,74 @@
+import TurndownService from "turndown";
+import { identity } from "@/utils";
+
+const turndownPluginGfm = require("joplin-turndown-plugin-gfm");
+
+export const usePluginAddRules = (turndownService, keeps) => {
+  // Use the gfm plugin
+  const { gfm } = turndownPluginGfm;
+  turndownService.use(gfm);
+
+  // We need a extra strikethrough rule because the strikethrough rule in gfm is single `~`.
+  turndownService.addRule("strikethrough", {
+    filter: ["del", "s", "strike"],
+    replacement(content) {
+      return "~~" + content + "~~";
+    },
+  });
+
+  turndownService.addRule("paragraph", {
+    filter: "p",
+
+    replacement: function (content, node) {
+      const isTaskListItemParagraph =
+        node.previousElementSibling &&
+        node.previousElementSibling.tagName === "INPUT";
+
+      return isTaskListItemParagraph
+        ? content + "\n\n"
+        : "\n\n" + content + "\n\n";
+    },
+  });
+
+  turndownService.addRule("listItem", {
+    filter: "li",
+
+    replacement: function (content, node, options) {
+      content = content
+        .replace(/^\n+/, "") // remove leading newlines
+        .replace(/\n+$/, "\n") // replace trailing newlines with just a single one
+        .replace(/\n/gm, "\n  "); // indent
+
+      let prefix = options.bulletListMarker + " ";
+      const parent = node.parentNode;
+      if (parent.nodeName === "OL") {
+        const start = parent.getAttribute("start");
+        const index = Array.prototype.indexOf.call(parent.children, node);
+        prefix = (start ? Number(start) + index : index + 1) + ". ";
+      }
+
+      return (
+        prefix +
+        content +
+        (node.nextSibling && !/\n$/.test(content) ? "\n" : "")
+      );
+    },
+  });
+
+  // Handle multiple math lines
+  turndownService.addRule("multiplemath", {
+    filter(node, options) {
+      return (
+        node.nodeName === "PRE" && node.classList.contains("multiple-math")
+      );
+    },
+    replacement(content, node, options) {
+      return `$$\n${content}\n$$`;
+    },
+  });
+
+  turndownService.escape = identity;
+  turndownService.keep(keeps);
+};
+
+export default TurndownService;
